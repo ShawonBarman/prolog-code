851068f5b80367635b295a25e9611d47618577cf
Shawon Barman <46573333+ShawonBarman@users.noreply.github.com>
2022-03-14 20:17:02 +0600

    code about male and female

---
+++ code for learning prolog/2_male_female.pl
@@ -0,0 +1,9 @@
+%%%%% male or female %%%%%
+%%%%% shawon is male %%%%%
+male(shawon).
+%%%%% anik is male %%%%%
+male(anik).
+%%%%% piu is female %%%%%
+female(piu).
+%%%%% munni is female %%%%%
+female(munni).
